116301c5f1c44094f3d8b163cb0e42701a5e904f
Karen Anhaia <karenanhaia@hotmail.com>
2018-05-15 19:43:13 -0300

    7 - Inclusão de logo homepage e ajuste de validações

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,5 @@
 class Meal < ApplicationRecord
-  validates_presence_of :description
+  validates_presence_of :description, :price, :productiontime
 
   has_and_belongs_to_many :ingredients
   belongs_to :restaurant
